379e6d7a42a3f7019919b1da5f5f2229302ae539
joshliford <j_liford1@yahoo.com>
2026-01-27 20:20:22 -0600

    updated all models using JPA annotations; added JPA repositories for each model; updated all controllers with appropriate methods; tested endpoints using Postman

--- src/main/java/com/example/springBoot2/controllers/MovieController.java
+++ src/main/java/com/example/springBoot2/controllers/MovieController.java
@@ -1,6 +1,7 @@
 package com.example.springBoot2.controllers;
 
 import com.example.springBoot2.models.Movie;
+import com.example.springBoot2.repositories.MovieRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -8,14 +9,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
-    private final List<Movie> movies = List.of(
-        new Movie("The Shawshank Redemption", "Frank Darabont", 1994, 142),
-        new Movie("The Godfather", "Francis Ford Coppola", 1972, 175),
-        new Movie("The Dark Knight", "Christopher Nolan", 2008, 152)
-    );
+    private final MovieRepository movieRepository;
 
-    @GetMapping
-    public List<Movie> getMovies() {
-        return movies;
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
     }
+
+    @GetMapping("")
+    public List<Movie> getAllBook() {
+        return movieRepository.findAll();
+    }
+
+    @GetMapping("/details/{id}")
+    public Movie getMovieById(@PathVariable int id) {
+        return movieRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping("/add")
+    public Movie addMovie(@RequestBody Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @PutMapping("/{id}/update")
+    public Movie updateMovie(@PathVariable int id, @RequestBody Movie movie) {
+        movie.setId(id);
+        return movieRepository.save(movie);
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public void deleteMovieById(@PathVariable int id) {
+        movieRepository.deleteById(id);
+    }
+
+
 }
--- src/main/java/com/example/springBoot2/models/Album.java
+++ src/main/java/com/example/springBoot2/models/Album.java
@@ -1,7 +1,17 @@
 package com.example.springBoot2.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     private String name;
     private int year;
     private String artist;
@@ -47,5 +57,9 @@ public class Album {
 
     public void setTracks(int tracks) {
         this.tracks = tracks;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
